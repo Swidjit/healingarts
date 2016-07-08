2fb28bd1a3d85e3239c81c22b29dd99cf8a71e07
Alex Colket <Alex@Alexs-MacBook-Pro.local>
2016-07-07 21:33:24 -0400

    getting started

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,17 @@
+class PagesController < ApplicationController
+
+  def home
+
+  end
+
+  def index
+    @title = params[:page_name].titleize
+
+    render params[:page_name]
+  end
+
+  def sitemap
+
+  end
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,4 @@
+Rails.application.routes.draw do
+  root :to => 'pages#home'
+  get '/pages/:page_name' => 'pages#index', :as => :pages
+end
